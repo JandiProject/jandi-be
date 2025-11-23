ba73dd1164fa2c4dfd6114bdf13de7856743e9f9
myoungjugo <rinaroo0512@naver.com>
2025-11-24 04:17:02 +0900

    [not yet tested]added mail_server
    메일 서버 main.py, consumer.py, email_service.py구현
    아직 rabbitmq서버가 없어 임시 주석처리, env에는 smtp발신 계정 정보 필요

--- mail_server/main.py
+++ mail_server/main.py
@@ -1,16 +1,59 @@
-import time
+# mail_server/main.py
+#워커진입점, api서버
+
+import uvicorn
+from fastapi import FastAPI
 import os
+import logging
+import asyncio 
+import threading 
+import time 
 
-# 컨테이너가 시작될 때 로그 출력
-print(f"🚀 Service Started: {os.getenv('HOSTNAME', 'Worker')}")
+# Pika컨슈머 로직
+from mail_server.consumer import start_pika_consumer 
 
-try:
-    # 무한 루프를 돌면서 프로세스가 죽지 않게 함
-    while True:
-        # 60초마다 한 번씩만 깨어남 (CPU 낭비 방지)
-        time.sleep(60)
-        # 살아있다는 생존 신고 로그 (선택 사항)
-        print("💤 Worker is idle... waiting for real code implementation.")
+# 로깅 설정
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-except KeyboardInterrupt:
-    print("🛑 Service Stopping...")
+# FastAPI 애플리케이션 인스턴스 생성
+app = FastAPI(
+    title="Mail Worker API",
+    version="1.0.0",
+)
+
+#pika 컨슈머 스레드 시작함
+#fast api가 비동기 메인 스레드라 그거 블로킹 안하려고 
+#동기 방식인 pika 컨슈머를 별도 백그라운드 스레드로 분리
+
+@app.on_event("startup")
+async def startup_event():
+    logger.info(f"🚀 Mail Worker is starting FastAPI server...")
+    #분리된 백그라운드 스레드로. 
+    #rabbit mq서버 아직 없어서 임시주석처리
+    #threading.Thread(target=start_pika_consumer, daemon=True).start()
+    logger.info("🔗 RabbitMQ Consumer started in a background thread.")
+
+#서버 종료 시 호출됨
+@app.on_event("shutdown")
+async def shutdown_event():
+    logger.info("🛑 Service Stopping...")
+    pass
+
+#상태 확인 엔드포인트
+#외부의 로드밸런서가 확인할 수 있다고 함
+@app.get("/health", tags=["status"])
+async def read_health():
+    return {"status": "ok", "message": "Mail Worker is running"}
+
+#프로그램 진입
+#uvicorn호출해서 fast api서버와 비동기 이벤트 루프 시작
+
+if __name__ == "__main__":
+    #서버구동
+    uvicorn.run(
+        "main:app", 
+        host="0.0.0.0", 
+        port=8000, 
+        reload=False 
+    )

ad8876eee76c2f306b77170d2690d42db1693bc3
jdk829355 <jdk829355@gmail.com>
2026-02-19 13:44:39 +0900

    refactor: import 방식 변경

--- main_server/app/routers/platform_router.py
+++ main_server/app/routers/platform_router.py
@@ -6,7 +6,7 @@ from app.models.user_models import UserPlatform, Platform
 from app.schemas.platform_schemas import UserPlatformRequest
 from app.core.verify_jwt import get_current_user_id
 from app.dependencies.rabbitmq import publish_message
-from app.services.platform_service import add_user_platform_mapping, delete_user_platform_mapping, get_platform_info, get_user_platforms, make_article_data
+from app.services import platform_service
 router = APIRouter(
     prefix="/api/platform",
     tags=["Platform"]
@@ -22,14 +22,14 @@ def register_platform(
     user_id: str = Depends(get_current_user_id)
     ):
     # 플랫폼 정보 조회
-    platform_info = get_platform_info(db, req.platform_name)
+    platform_info = platform_service.get_platform_info(db, req.platform_name)
 
     # 유저-플랫폼 매핑 추가 또는 업데이트
-    add_user_platform_mapping(db, user_id, platform_info.platform_id, req.account_id)
+    platform_service.add_user_platform_mapping(db, user_id, platform_info.platform_id, req.account_id)
 
     # 메시지큐에 넣을 데이터 생성 (궁극적으로 이 부분은 없어지는 게 나아보임)
     data = []
-    make_article_data(data, req.platform_name, req.account_id, user_id)
+    platform_service.make_article_data(data, req.platform_name, req.account_id, user_id)
 
     # TODO: 게시글 데이터를 통째로 MQ로 보내는 것은 비효율적임. 플랫폼 정보만 발행하는 게 나아보임. 그러면 main server에서 rss 파싱을 안해도 됨
     publish_message("platform_register", data)
@@ -43,12 +43,12 @@ def delete_platform(
     user_id: str = Depends(get_current_user_id)
     ):
 
-    platform_info = get_platform_info(db, req.platform_name)
+    platform_info = platform_service.get_platform_info(db, req.platform_name)
 
     if not platform_info:
         raise HTTPException(status_code=404, detail=f"지원하지 않는 플랫폼: {req.platform_name}")
     
-    delete_user_platform_mapping(logger, db, user_id, platform_info.platform_id, req.account_id)
+    platform_service.delete_user_platform_mapping(logger, db, user_id, platform_info.platform_id, req.platform_name)
 
     return {
         "message": "삭제 완료"
@@ -59,5 +59,5 @@ def get_platforms(
     db: Session = Depends(get_db),
     user_id: str = Depends(get_current_user_id)
     ):
-    res = get_user_platforms(db, user_id)
+    res = platform_service.get_user_platforms(db, user_id)
     return res

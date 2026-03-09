1c55a4a7db4cb5c79c5693d1bc0c8763b12facee
jdk829355 <jdk829355@gmail.com>
2026-03-09 20:28:52 +0900

    fix: externalpost.id 타입 변경 및 db 마이그레이션 새로 작성

--- main_server/app/models/trend_models.py
+++ main_server/app/models/trend_models.py
@@ -12,7 +12,7 @@ class Keyword(Base):
 
 class ExternalPost(Base):
     __tablename__ = 'EXTERNAL_POSTS'
-    id = Column(Uuid(as_uuid=True), primary_key=True) # URL 해시값
+    id = Column(String(100), primary_key=True) # URL 해시값 -> string임
     source = Column(String(50))               # 기업명
     title = Column(String(255))               # 글 제목
     url = Column(Text)
@@ -29,7 +29,7 @@ class ExternalPost(Base):
 class PostKeywordMapping(Base):
     __tablename__ = 'EXTERNAL_POSTS_KEYWORDS'
     keyword_id = Column(Integer, ForeignKey('KEYWORDS.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key=True)
-    post_id = Column(Uuid(as_uuid=True), ForeignKey('EXTERNAL_POSTS.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key=True)
+    post_id = Column(String(100), ForeignKey('EXTERNAL_POSTS.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key=True)
 
 # MATERIALIZED VIEW
 class TrendingKeywordView(ViewBase):
@@ -45,7 +45,7 @@ class ArticlesMentioningKeywordsView(ViewBase):
     __tablename__ = 'ARTICLES_MENTIONING_KEYWORDS_VIEW'
     __table_args__ = {'info': {'is_view': True}}
     
-    id = Column(Uuid(as_uuid=True), primary_key=True, nullable=False)
+    id = Column(String(100), primary_key=True, nullable=False)
     title = Column(String(255), nullable=False)
     url = Column(Text, nullable=False)
     source = Column(String(100), nullable=False)
